62717e0051b7239db25eb4817cc2042ba728f8b0
Christopher Nathanael Tessy <christopher.nathanael1217@gmail.com>
2025-03-21 10:14:54 +0700

    feat: add trending card hover effect

--- src/app/components/home/trending-section/TrendingCard.tsx
+++ src/app/components/home/trending-section/TrendingCard.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Ratings from "../../Ratings";
+import Link from "next/link";
 
 interface TrendingCardProps {
   backgroundColor: string;
@@ -17,7 +18,19 @@ export default function TrendingCard({
   rating,
 }: TrendingCardProps) {
   return (
-    <div className={`${backgroundColor} flex flex-col gap-4 p-8 rounded-xl`}>
+    <Link
+      href={"/"}
+      className={`${backgroundColor} flex flex-col gap-4 p-8 rounded-xl group relative`}
+    >
+      <div className="hidden absolute w-full h-full inset-0 m-auto opacity-5 group-hover:block">
+        <Image
+          src={"/images/home/background.png"}
+          alt="Doodle of foods"
+          fill
+          className="object-cover"
+        />
+      </div>
+
       <div className="w-32 h-32 rounded-md overflow-hidden relative">
         <Image src={image} alt={name} fill className="object-cover" />
       </div>
@@ -28,6 +41,6 @@ export default function TrendingCard({
       </div>
 
       <Ratings rating={rating} />
-    </div>
+    </Link>
   );
 }
